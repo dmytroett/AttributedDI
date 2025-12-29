52e7044c1b57e648d5c4593c493130e8909a6cfb
Dmytro.Ett <dmytro.ett@outlook.com>
2025-12-29 22:11:46 +0100

    Perfect

--- src/AttributedDI.SourceGenerator/IsExternalInit.cs
+++
@@ -1,9 +0,0 @@
-// This file provides polyfill for C# 9+ record types in netstandard2.0
-
-#if NETSTANDARD2_0
-namespace System.Runtime.CompilerServices
-{
-    internal static class IsExternalInit { }
-}
-#endif
-
