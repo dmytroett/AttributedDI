3eb24b1a8cd903b9d9b84ce8162f09866fbbb629
Dmytro.Ett <dmytro.ett@outlook.com>
2025-12-31 20:04:03 +0100

    Starting refactoring journey

--- src/AttributedDI.SourceGenerator/Strategies/ServiceRegistrationStrategy.cs
+++ src/AttributedDI.SourceGenerator/Strategies/ServiceRegistrationStrategy.cs
@@ -150,7 +150,7 @@ internal static class ServiceRegistrationStrategy
         if (registrations.Count == 0)
             return null;
 
-        return new TypeWithAttributesInfo(symbol, registrations.ToImmutable());
+        return new TypeWithAttributesInfo(symbol, registrations.MoveToImmutable());
     }
 
     private static string ResolveLifetime(List<AttributeData> lifetimeAttributes)
