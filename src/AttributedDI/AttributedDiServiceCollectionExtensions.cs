39b9bfd33157f0585b4e96be4b2bfc17757bccdb
Dmytro.Ett <dmytro.ett@outlook.com>
2025-12-31 17:36:41 +0100

    Extensions

---
+++ src/AttributedDI/AttributedDiServiceCollectionExtensions.cs
@@ -0,0 +1,84 @@
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+
+namespace AttributedDI;
+
+/// <summary>
+/// Provides extension methods for <see cref="IServiceCollection"/> to register service modules.
+/// </summary>
+public static class AttributedDiServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds a service module to the service collection.
+    /// </summary>
+    /// <param name="services">The service collection to add the module to.</param>
+    /// <param name="module">The service module to register.</param>
+    /// <returns>The service collection for chaining.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> or <paramref name="module"/> is null.</exception>
+    public static IServiceCollection AddModule(this IServiceCollection services, IServiceModule module)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(module);
+
+        module.ConfigureServices(services);
+        return services;
+    }
+
+    /// <summary>
+    /// Adds a service module of type <typeparamref name="TModule"/> to the service collection.
+    /// </summary>
+    /// <typeparam name="TModule">The type of service module to register. Must have a parameterless constructor.</typeparam>
+    /// <param name="services">The service collection to add the module to.</param>
+    /// <returns>The service collection for chaining.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> is null.</exception>
+    /// <exception cref="MissingMethodException">Thrown when <typeparamref name="TModule"/> does not have a parameterless constructor.</exception>
+    public static IServiceCollection AddModule<TModule>(this IServiceCollection services)
+        where TModule : IServiceModule, new()
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        var module = new TModule();
+        return services.AddModule(module);
+    }
+
+    /// <summary>
+    /// Adds multiple service modules to the service collection.
+    /// </summary>
+    /// <param name="services">The service collection to add the modules to.</param>
+    /// <param name="modules">The service modules to register.</param>
+    /// <returns>The service collection for chaining.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> or <paramref name="modules"/> is null.</exception>
+    public static IServiceCollection AddModules(this IServiceCollection services, params IServiceModule[] modules)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(modules);
+
+        foreach (var module in modules)
+        {
+            services.AddModule(module);
+        }
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds multiple service modules to the service collection.
+    /// </summary>
+    /// <param name="services">The service collection to add the modules to.</param>
+    /// <param name="modules">The service modules to register.</param>
+    /// <returns>The service collection for chaining.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> or <paramref name="modules"/> is null.</exception>
+    public static IServiceCollection AddModules(this IServiceCollection services, IEnumerable<IServiceModule> modules)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(modules);
+
+        foreach (var module in modules)
+        {
+            services.AddModule(module);
+        }
+
+        return services;
+    }
+}
