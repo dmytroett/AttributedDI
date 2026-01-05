57b6b39790ec6ddb252b6aae14abbe1aeb7d331a
Dmytro Ett <dmytro.ett@outlook.com>
2026-01-05 01:46:10 +0100

    We're inching forward

--- test/AttributedDI.IntegrationTests/AllModulesRegistrationTests.cs
+++ test/AttributedDI.IntegrationTests/AllModulesRegistrationTests.cs
@@ -1,4 +1,5 @@
-using System;
+using Microsoft.Extensions.DependencyInjection;
+using AllModulesRegistration.Core;
 
 namespace AttributedDI.IntegrationTests;
 
@@ -8,6 +9,11 @@ public class AllModulesRegistrationTests
     [Fact]
     public void AllServicesRegisteredCorrectly()
     {
-        
+        ServiceCollection services = new ();
+
+        services.AddAttributedDi();
+
+        AssertContainsService<IMyAmazingService, MyAmazingService>(services, ServiceLifetime.Transient);
+        AssertContainsService<IInternalService, InternalService>(services, ServiceLifetime.Transient);
     }
 }
